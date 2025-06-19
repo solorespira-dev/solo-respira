3c801c3e428d4387082afb814e50260816ec36df
Default-php <alfonsogonzalezarellano22@gmail.com>
2025-06-18 22:17:47 -0400

    chore: elimina del seguimiento archivos ignorados (conexion.local.php, conexion.remoto.php)

--- solo-respira/config/conexion.remoto.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Configuración para entorno REMOTO (InfinityFree)
-$host = "sql303.infinityfree.com";
-$usuario = "if0_38704646";
-$contraseña = "Solorespira123";
-$base_datos = "if0_38704646_SoloRespiraDB";
-?>
